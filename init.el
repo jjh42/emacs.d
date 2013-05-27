f5393ce3759f401ef8016423526600af38806dda
Jonathan J Hunt <j@me.net.nz>
2013-05-27 15:40:11 -0700

    Stop junk backup files.

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-elixir")
 (require 'elixir-mode-setup)
 (elixir-mode-setup)
+
+;; ========== Prevent Emacs from making backup files ==========
+(setq make-backup-files nil)
