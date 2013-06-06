ada4b8cec422c49a6ddfb65b178792694ff48fa3
Jonathan J Hunt <j@me.net.nz>
2013-06-06 16:31:23 -0700

    Added editorconfig Setup python mode Prevented backup files.

--- init.el
+++ init.el
@@ -1,6 +1,27 @@
+;; My emacs config.
+;; (C) 2013 Jonathan J Hunt
+;; License under MIT license.
+;; Submodules belong to others and may involve different licenses.
+
+;; No backup files to clutter things up.
+(setq make-backup-files nil) 
+
+;; Default tab width of 4 (8 is crazy)
+(setq default-tab-width 4)
+(add-hook 'python-mode-hook
+      (lambda ()
+        (setq tab-width 4)
+        (setq python-indent 4)))
+
+;; Elixir-lang mode
 (add-to-list 'load-path "~/.emacs.d/emacs-elixir")
 (require 'elixir-mode-setup)
 (elixir-mode-setup)
+
+;; Editorconfig
+(add-to-list 'load-path "~/.emacs.d/editorconfig-emacs")
+(load "editorconfig")
+
 
 ;; ========== Prevent Emacs from making backup files ==========
 (setq make-backup-files nil)
